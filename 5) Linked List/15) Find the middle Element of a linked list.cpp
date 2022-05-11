646007f83cd429a413dfde35512f77ec3d4e8bfb
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-05-11 18:02:19 +0530

    Add files via upload

---
+++ 5) Linked List/15) Find the middle Element of a linked list.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node *next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+};
+
+Node *start = NULL;
+
+Node * createNode() {
+    int val;
+    cout<<"Enter the Value: ";
+    cin>>val;
+    Node *n = new Node(val);
+    return n;
+}
+
+void insertNode() {
+    Node *n = createNode();
+    if(start == NULL) {
+        start = n;
+    }
+    else {
+        Node *temp = start;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = n;
+    }
+}
+
+//you have to complete this function
+Node* middleNode(Node* head) {
+    if(head == NULL || head->next == NULL) return head;
+    Node* slow = head, *fast = head;
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+void printElements() {
+    if(start == NULL) {
+        cout<<"List is Empty!";
+    }
+    else {
+        Node *temp = start;
+        cout<<"Data of the List is: ";
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+}
+
+void menu() {
+    cout<<"\n\nPress 1 -> Inserting the data";
+    cout<<"\nPress 3 -> Find the Mid of Linked Lists";
+    cout<<"\nPress 9 -> Print the data";
+    cout<<"\nPress 0 -> Exit!\n";
+}
+
+int main() {
+    while(1) {
+        menu();
+        int ch;
+        cout<<"\nEnter your choice: ";
+        cin>>ch;
+        switch (ch) {
+            case 1: {
+                insertNode();
+                break;
+            }
+            case 3: {
+                start = findMid(start);
+                printElements(start);
+                break;
+            }
+            case 9: {
+                printElements();
+                break;
+            }
+            case 0: {
+                cout<<"Program Terminatted Successfully!!"<<endl;
+                return 0;
+            }
+            default: {
+                cout<<"Invalid Choice!";
+                break;
+            }
+        }
+    }
+    return 0;
+}
