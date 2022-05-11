b309230ea597103f33ed58eb54a38835136cbc51
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-05-11 18:08:32 +0530

    Add files via upload

---
+++ 5) Linked List/16) Check if a linked list is a circular linked list.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node *next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+};
+
+Node *start = NULL;
+
+Node * createNode() {
+    int val;
+    cout<<"Enter the Value: ";
+    cin>>val;
+    Node *n = new Node(val);
+    return n;
+}
+
+void insertNode() {
+    Node *n = createNode();
+    if(start == NULL) {
+        start = n;
+    }
+    else {
+        Node *temp = start;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = n;
+    }
+}
+
+bool isCircular(Node *head) {
+   // Your code here
+   if(head == NULL) return false;
+   Node *slowPtr = head, *fastPtr = head->next;
+   while(slowPtr != fastPtr) {
+       if(slowPtr == NULL || fastPtr == NULL || fastPtr->next == NULL) {
+           return false;
+       }
+       slowPtr = slowPtr->next;
+       fastPtr = fastPtr->next->next;
+   }
+   return true;
+}
+
+void printElements() {
+    if(start == NULL) {
+        cout<<"List is Empty!";
+    }
+    else {
+        Node *temp = start;
+        cout<<"Data of the List is: ";
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+}
+
+void menu() {
+    cout<<"\n\nPress 1 -> Inserting the data";
+    cout<<"\nPress 3 -> Check whether is list is Circular or not?";
+    cout<<"\nPress 9 -> Print the data";
+    cout<<"\nPress 0 -> Exit!\n";
+}
+
+int main() {
+    while(1) {
+        menu();
+        int ch;
+        cout<<"\nEnter your choice: ";
+        cin>>ch;
+        switch (ch) {
+            case 1: {
+                insertNode();
+                break;
+            }
+            case 3: {
+                bool result = isCircular(start);
+                cout<<"\nList is circular or not? "<<result;
+                break;
+            }
+            case 9: {
+                printElements();
+                break;
+            }
+            case 0: {
+                cout<<"Program Terminatted Successfully!!"<<endl;
+                return 0;
+            }
+            default: {
+                cout<<"Invalid Choice!";
+                break;
+            }
+        }
+    }
+    return 0;
+}
