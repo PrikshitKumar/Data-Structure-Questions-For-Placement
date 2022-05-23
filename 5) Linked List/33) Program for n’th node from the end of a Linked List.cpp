525e7fbb589297be539a235cc76d8ea772f7d81a
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-05-23 22:36:42 +0530

    Add files via upload

---
+++ 5) Linked List/33) Program for n’th node from the end of a Linked List.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node *next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+};
+
+Node *start = NULL;
+
+Node * createNode() {
+    int val;
+    cout<<"Enter the Value: ";
+    cin>>val;
+    Node *n = new Node(val);
+    return n;
+}
+
+void insertNode() {
+    Node *n = createNode();
+    if(start == NULL) {
+        start = n;
+    }
+    else {
+        Node *temp = start;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = n;
+        n->prev = temp;
+    }
+}
+
+int getNthFromLast(Node *head, int n) {
+    Node *temp = head;
+    int count = 0;
+    
+    while(temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    
+    count = count - n;
+    if(count < 0) return -1;
+    
+    temp = head;
+    while(count--) {
+        temp = temp->next;
+    }
+    return temp->data;
+}
+
+void printElements(Node* head) {
+    if(head == NULL) {
+        cout<<"List is Empty!";
+    }
+    else {
+        Node *temp = head;
+        cout<<"Data of the List is: ";
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+}
+
+void menu() {
+    cout<<"\n\nPress 1 -> Inserting the data";
+    cout<<"\nPress 3 -> Find the nth Node from last of the List";
+    cout<<"\nPress 9 -> Print the data";
+    cout<<"\nPress 0 -> Exit!\n";
+}
+
+int main() {
+    while(1) {
+        menu();
+        int ch;
+        cout<<"\nEnter your choice: ";
+        cin>>ch;
+        switch (ch) {
+            case 1: {
+                insertNode();
+                break;
+            }
+            case 3: {
+                int result = getNthFromLast(start, 2);
+                cout<<"\n7th Node from the last of List is: "<<result;
+                break;
+            }
+            case 9: {
+                printElements(start);
+                break;
+            }
+            case 0: {
+                cout<<"Program Terminatted Successfully!!"<<endl;
+                return 0;
+            }
+            default: {
+                cout<<"Invalid Choice!";
+                break;
+            }
+        }
+    }
+    return 0;
+}
