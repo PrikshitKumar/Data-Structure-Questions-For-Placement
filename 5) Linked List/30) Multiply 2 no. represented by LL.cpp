686be699bb44f10b0b1b34c2dbde7a424751b5b3
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-05-23 12:53:21 +0530

    Add files via upload

---
+++ 5) Linked List/30) Multiply 2 no. represented by LL.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node *next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
+        }
+};
+
+Node *start = NULL;
+
+Node * createNode() {
+    int val;
+    cout<<"Enter the Value: ";
+    cin>>val;
+    Node *n = new Node(val);
+    return n;
+}
+
+void insertNode() {
+    Node *n = createNode();
+    if(start == NULL) {
+        start = n;
+    }
+    else {
+        Node *temp = start;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = n;
+        n->prev = temp;
+    }
+}
+
+long long  multiplyTwoLists (Node* l1, Node* l2) {
+    //Your code here
+    long long  num1=0 , num2=0;
+    long long  mod = 1000000007;
+    
+    while(l1) {
+        num1 *= 10;
+        num1 = num1%mod + l1->data;
+        l1 = l1->next;
+    }
+    while(l2) {
+        num2 *= 10;
+        num2 = num2%mod + l2->data;
+        l2 = l2->next;
+    }
+    return num1%mod * num2%mod;
+}
+
+void printElements(Node* head) {
+    if(head == NULL) {
+        cout<<"List is Empty!";
+    }
+    else {
+        Node *temp = head;
+        cout<<"Data of the List is: ";
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+}
+
+void menu() {
+    cout<<"\n\nPress 1 -> Inserting the data";
+    cout<<"\nPress 3 -> Multiplication of the List";
+    cout<<"\nPress 9 -> Print the data";
+    cout<<"\nPress 0 -> Exit!\n";
+}
+
+int main() {
+    while(1) {
+        menu();
+        int ch;
+        cout<<"\nEnter your choice: ";
+        cin>>ch;
+        switch (ch) {
+            case 1: {
+                insertNode();
+                break;
+            }
+            case 3: {
+                long long result = multiplyTwoLists(start, start);
+                cout<<"\nMultiplication of Two Linked List is: "<<result;
+                // printElements(start);
+                break;
+            }
+            case 9: {
+                printElements(start);
+                break;
+            }
+            case 0: {
+                cout<<"Program Terminatted Successfully!!"<<endl;
+                return 0;
+            }
+            default: {
+                cout<<"Invalid Choice!";
+                break;
+            }
+        }
+    }
+    return 0;
+}
