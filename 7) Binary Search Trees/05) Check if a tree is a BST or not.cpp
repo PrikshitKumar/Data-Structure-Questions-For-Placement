62be77b76fb836eafe9d9895cf20ae29d4950daf
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-06-26 17:28:50 +0530

    Add files via upload

---
+++ 7) Binary Search Trees/05) Check if a tree is a BST or not.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left, *right;
+        Node(int data) {
+            this->data = data;
+            left = right = NULL;
+        }
+};
+
+void inorder(Node *root, vector<int> &vec) {
+    if(!root) return;
+    inorder(root->left, vec);
+    vec.push_back(root->data);
+    inorder(root->right, vec);
+}
+
+bool isBST(Node* root) {
+    vector<int> vec;
+    inorder(root, vec);
+    for(int i=1; i<vec.size(); i++) {
+        if(vec[i-1] > vec[i]) return 0; 
+    }
+    return 1;
+}
+
+vector<int> levelOrder(Node* node) {
+    vector<int> ans;
+    if(node==NULL) return ans;
+    
+    queue<Node *> q;
+    q.push(node);
+    
+    while(!q.empty()) {
+        Node *front = q.front();
+        ans.push_back(front->data);
+        q.pop();
+        
+        if(front->left != NULL) {
+            q.push(front->left);
+        }
+        if(front->right != NULL) {
+            q.push(front->right);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    Node* root = new Node(8);
+    root->left = new Node(3);
+    root->right = new Node(10);
+    root->left->left = new Node(1);
+    root->left->right = new Node(6);
+    root->left->right->left = new Node(4);
+    root->left->right->right = new Node(7);
+    root->right->right = new Node(14);
+    root->right->right->left = new Node(13);
+ 
+    vector<int> result = levelOrder(root);
+    cout<<"Values after Level-Order Traversal are: "<<endl;
+    for(int i=0; i<result.size(); i++) cout<<result[i]<<" ";
+
+    cout<<"\nGiven Tree is a BST or Not? "<<isBST(root);
+    return 0;
+}
