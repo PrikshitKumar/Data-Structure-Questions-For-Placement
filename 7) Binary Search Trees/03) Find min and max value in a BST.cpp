df7db0858764038ca7d7944487b388cf366f3061
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-06-26 16:57:11 +0530

    Add files via upload

---
+++ 7) Binary Search Trees/03) Find min and max value in a BST.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left, *right;
+        Node(int data) {
+            this->data = data;
+            left = right = NULL;
+        }
+};
+
+Node *findMin(Node *root) {
+    if(root->left == NULL) return root;
+    return findMin(root->left);
+}
+
+Node *findMax(Node *root) {
+    if(root->right == NULL) return root;
+    return findMax(root->right);
+}
+
+vector<int> levelOrder(Node* node) {
+    vector<int> ans;
+    if(node==NULL) return ans;
+    
+    queue<Node *> q;
+    q.push(node);
+    
+    while(!q.empty()) {
+        Node *front = q.front();
+        ans.push_back(front->data);
+        q.pop();
+        
+        if(front->left != NULL) {
+            q.push(front->left);
+        }
+        if(front->right != NULL) {
+            q.push(front->right);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    Node* root = new Node(8);
+    root->left = new Node(3);
+    root->right = new Node(10);
+    root->left->left = new Node(1);
+    root->left->right = new Node(6);
+    root->left->right->left = new Node(4);
+    root->left->right->right = new Node(7);
+    root->right->right = new Node(14);
+    root->right->right->left = new Node(13);
+ 
+    vector<int> result = levelOrder(root);
+    cout<<"Values after Level-Order Traversal are: "<<endl;
+    for(int i=0; i<result.size(); i++) cout<<result[i]<<" ";
+
+    cout<<"\nMinimum Element of the BST is: "<<findMin(root)->data;
+    cout<<"\nmaximum Element of the BST is: "<<findMax(root)->data;
+    return 0;
+}
