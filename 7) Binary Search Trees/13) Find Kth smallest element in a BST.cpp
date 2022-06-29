728cea8fc9521a116f9d23736fd09e89bc325d18
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-06-29 18:52:08 +0530

    Add files via upload

---
+++ 7) Binary Search Trees/13) Find Kth smallest element in a BST.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left, *right;
+        Node(int data) {
+            this->data = data;
+            left = right = NULL;
+        }
+};
+
+Node *search(Node *root, int &k) {
+    if(!root) return NULL;
+    Node *left = search(root->left, k);
+    if(left != NULL) return left;
+    k--;
+    if(k==0) return root;
+    Node *right = search(root->right, k);
+    return right;
+}
+
+int kthSmallest(Node *root, int k) {
+    Node *result = search(root, k);
+    if(!result || k!=0) return -1;
+    return result->data;
+}
+
+vector<int> levelOrder(Node* node) {
+    vector<int> ans;
+    if(node==NULL) return ans;
+    
+    queue<Node *> q;
+    q.push(node);
+    
+    while(!q.empty()) {
+        Node *front = q.front();
+        ans.push_back(front->data);
+        q.pop();
+        
+        if(front->left != NULL) {
+            q.push(front->left);
+        }
+        if(front->right != NULL) {
+            q.push(front->right);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    Node* root = new Node(8);
+    root->left = new Node(3);
+    root->right = new Node(10);
+    root->left->left = new Node(1);
+    root->left->right = new Node(6);
+    root->left->right->left = new Node(4);
+    root->left->right->right = new Node(7);
+    root->right->right = new Node(14);
+    root->right->right->left = new Node(13);
+ 
+    vector<int> result = levelOrder(root);
+    cout<<"Values after Level-Order Traversal are: "<<endl;
+    for(int i=0; i<result.size(); i++) cout<<result[i]<<" ";
+
+    cout<<"\n3rd Smallest Element in a BST is: "<<kthSmallest(root, 3);
+    return 0;
+}
