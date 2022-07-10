d2ac3b9d22209fbb694b69c946dde18ee75d2acf
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-07-10 12:15:25 +0530

    Add files via upload

---
+++ 10) Stacks & Queues/05) Implement 'N' stacks in an Array.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// URL: https://www.geeksforgeeks.org/efficiently-implement-k-stacks-single-array/
+class kStacks {
+	int *arr; // Array of size n to store original data
+	int *top; // Array of size k to store indexes of top elements of stacks
+	int *next; // Array of size n to store next entry in all stacks and free list
+	int n, k;
+	int free; // To store beginning index of free list
+
+public:
+	//constructor to create k stacks in an array of size n
+	kStacks(int k, int n);
+
+	// A utility function to check if there is space available
+	bool isFull() { return (free == -1); }
+
+	// To push an item in stack number 'sn' where sn is from 0 to k-1
+	void push(int item, int sn);
+
+	// To pop an from stack number 'sn' where sn is from 0 to k-1
+	int pop(int sn);
+
+	// To check whether stack number 'sn' is empty or not
+	bool isEmpty(int sn) { return (top[sn] == -1); }
+};
+
+//constructor to create k stacks in an array of size n
+kStacks::kStacks(int k1, int n1) {
+	// Initialize n and k, and allocate memory for all arrays
+	k = k1, n = n1;
+	arr = new int[n];
+	top = new int[k];
+	next = new int[n];
+
+	// Initialize all stacks as empty
+	for (int i=0; i<k; i++) {
+		top[i] = -1;
+    }
+
+	// Initialize all spaces as free
+	free = 0;
+	for (int i=0; i<n-1; i++) {
+		next[i] = i+1;
+    }
+	next[n-1] = -1; // -1 is used to indicate end of free list
+}
+
+// To push an item in stack number 'sn' where sn is from 0 to k-1
+void kStacks::push(int item, int sn) {
+	// Overflow check
+	if(isFull()) {
+		cout << "\nStack Overflow\n";
+		return;
+	}
+
+	int index = free;	 // Store index of first free slot
+
+	// Update index of free slot to index of next slot in free list
+	free = next[index];
+
+	// Update next of top and then top for stack number 'sn'
+	next[index] = top[sn];
+	top[sn] = index;
+
+	// Put the item in array
+	arr[index] = item;
+}
+
+// To pop an from stack number 'sn' where sn is from 0 to k-1
+int kStacks::pop(int sn) {
+	// Underflow check
+	if (isEmpty(sn)) {
+		cout << "\nStack Underflow\n";
+		return INT_MAX;
+	}
+
+	// Find index of top item in stack number 'sn'
+	int index = top[sn];
+
+	top[sn] = next[index]; // Change top to store next of previous top
+
+	// Attach the previous top to the beginning of free list
+	next[index] = free;
+	free = index;
+
+	// Return the previous top item
+	return arr[index];
+}
+
+int main() {
+	// Let us create 3 stacks in an array of size 10
+	int k = 3, n = 10;
+	kStacks ks(k, n);
+
+	// Let us put some items in stack number 2
+	ks.push(15, 2);
+	ks.push(45, 2);
+
+	// Let us put some items in stack number 1
+	ks.push(17, 1);
+	ks.push(49, 1);
+	ks.push(39, 1);
+
+	// Let us put some items in stack number 0
+	ks.push(11, 0);
+	ks.push(9, 0);
+	ks.push(7, 0);
+
+	cout << "Popped element from stack 2 is " << ks.pop(2) << endl;
+	cout << "Popped element from stack 1 is " << ks.pop(1) << endl;
+	cout << "Popped element from stack 0 is " << ks.pop(0) << endl;
+
+	return 0;
+}
