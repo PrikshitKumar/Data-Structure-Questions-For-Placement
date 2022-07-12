a2ef1994f7d2fd909b8ca5db2116d8a852bc8efb
Prikshit Kumar <64913497+PrikshitKumar@users.noreply.github.com>
2022-07-12 22:50:34 +0530

    Add files via upload

---
+++ 10) Stacks & Queues/14) Reverse a stack using recursion.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+stack<char> st;
+string ns;
+
+void insert_at_bottom(char x) {
+	if(st.size() == 0) st.push(x);
+	else {
+		char a = st.top();
+		st.pop();
+		insert_at_bottom(x);
+        st.push(a);
+	}
+}
+
+void reverse() {
+	if(st.size() > 0) {
+		char x = st.top();
+		st.pop();
+		reverse();
+		insert_at_bottom(x);
+	}
+}
+
+int main() {	
+    st.push('1');
+	st.push('2');
+	st.push('3');
+	st.push('4');
+	
+	cout<<"Original Stack is: 1 2 3 4"<<endl;
+	reverse();
+	cout<<"Reversed Stack is: ";
+	while(!st.empty()) {
+		char p = st.top();
+		st.pop();
+		ns += p;
+	}
+	cout<<ns[3]<<" "<<ns[2]<<" "<<ns[1]<<" "<<ns[0]<<endl;
+	return 0;
+}
